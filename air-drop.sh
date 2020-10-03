a9ed2d7ca64d46b05842eb48d03e6d3bc92a77d2
Srinibas Misra <srinibas.misra97@gmail.com>
2020-10-03 08:07:06 +0530

    added startup script

---
+++ air-drop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS=/home/srinibasmisra/air-drop/webserver/service-accounts/download-gcs.json
+/home/srinibasmisra/air-drop/venv/bin/python /home/srinibasmisra/air-drop/webserver/server.py
